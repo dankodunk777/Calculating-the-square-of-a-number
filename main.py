4a966323cc50bd0775e7eccbc9f9ad76f273e611
michelle88wg4p6 <138007559+michelle88wg4p6@users.noreply.github.com>
2023-06-28 15:51:04 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+Here's an example of a Python script that calculates the square of a number:
+
+```python
+number = float(input("Enter a number: "))
+square = number ** 2
+print("The square of", number, "is", square)
+```
+
+And here's an example of JavaScript code that calculates the square of a number:
+
+```javascript
+let number = parseFloat(prompt("Enter a number: "));
+let square = number ** 2;
+console.log("The square of", number, "is", square);
+```
+
+Both examples prompt the user to enter a number, calculate its square, and then display the result.
